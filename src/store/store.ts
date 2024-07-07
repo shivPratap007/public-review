535ed2c3b2aef00eefb56914a15c8be80321e548
Shiv Pratap <sp09singhwaghel@gmail.com>
2024-07-07 22:35:02 +0530

    Add: locally storing data functionality

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,4 +1,5 @@
 import { create } from "zustand";
+import { persist } from "zustand/middleware";
 import { FeedBackItemInterface } from "../lib/types";
 
 interface StoreState {
@@ -15,60 +16,72 @@ interface StoreState {
   fetchFeedback: () => Promise<void>;
 }
 
-const useStore = create<StoreState>((set, get) => ({
-  feedBackItems: [],
-  isLoading: false,
-  errorMessage: "",
-  selectedCompany: "All",
-  companies: [],
-  filterCompanies: [],
-  getCompanyList: () => {
-    const { feedBackItems } = get();
-    return feedBackItems
-      .map((item) => item.company)
-      .filter((company, index, array) => array.indexOf(company) === index);
-  },
-  getFilteredCompanyList: () => {
-    const { feedBackItems, selectedCompany } = get();
-    return selectedCompany === "All"
-      ? feedBackItems
-      : feedBackItems.filter((item) => item.company === selectedCompany);
-  },
-  handleAddToList: (text: string) => {
-    const companyNameFromText: string =
-      text
-        ?.split(" ")
-        ?.find((word) => word.includes("#"))
-        ?.substring(1) ?? "";
-    const newItem: FeedBackItemInterface = {
-      id: new Date().getTime(),
-      upvoteCount: 0,
-      badgeLetter: companyNameFromText.substring(0, 1).toUpperCase(),
-      company: companyNameFromText,
-      text: text,
-      daysAgo: 0,
-    };
-    set((state) => ({ feedBackItems: [...state.feedBackItems, newItem] }));
-  },
-  handleSelectedCompany: (text: string) => {
-    set(() => ({ selectedCompany: text }));
-  },
-  fetchFeedback: async () => {
-    try {
-      set({ isLoading: true, errorMessage: "" });
-      const response = await fetch(
-        "https://bytegrad.com/course-assets/projects/corpcomment/api/feedbacks"
-      );
-      if (!response.ok) {
-        throw new Error("Network response was not ok");
-      }
-      const data = await response.json();
-      console.log(data);
-      set({ feedBackItems: data.feedbacks, isLoading: false });
-    } catch (error) {
-      set({ errorMessage: "Network response was not ok", isLoading: false });
+const useStore = create<StoreState>()(
+  persist(
+    (set, get) => ({
+      feedBackItems: [],
+      isLoading: false,
+      errorMessage: "",
+      selectedCompany: "All",
+      companies: [],
+      filterCompanies: [],
+      getCompanyList: () => {
+        const { feedBackItems } = get();
+        return feedBackItems
+          .map((item) => item.company)
+          .filter((company, index, array) => array.indexOf(company) === index);
+      },
+      getFilteredCompanyList: () => {
+        const { feedBackItems, selectedCompany } = get();
+        return selectedCompany === "All"
+          ? feedBackItems
+          : feedBackItems.filter((item) => item.company === selectedCompany);
+      },
+      handleAddToList: (text: string) => {
+        const companyNameFromText: string =
+          text
+            ?.split(" ")
+            ?.find((word) => word.includes("#"))
+            ?.substring(1) ?? "";
+        const newItem: FeedBackItemInterface = {
+          id: new Date().getTime(),
+          upvoteCount: 0,
+          badgeLetter: companyNameFromText.substring(0, 1).toUpperCase(),
+          company: companyNameFromText,
+          text: text,
+          daysAgo: 0,
+        };
+        set((state) => ({ feedBackItems: [...state.feedBackItems, newItem] }));
+      },
+      handleSelectedCompany: (text: string) => {
+        set(() => ({ selectedCompany: text }));
+      },
+      fetchFeedback: async () => {
+        if (get().feedBackItems.length > 0) {
+          return; 
+        }
+
+        try {
+          set({ isLoading: true, errorMessage: "" });
+          const response = await fetch(
+            "https://bytegrad.com/course-assets/projects/corpcomment/api/feedbacks"
+          );
+          if (!response.ok) {
+            throw new Error("Network response was not ok");
+          }
+          const data = await response.json();
+          console.log(data);
+          set({ feedBackItems: data.feedbacks, isLoading: false });
+        } catch (error) {
+          set({ errorMessage: "Network response was not ok", isLoading: false });
+        }
+      },
+    }),
+    {
+      name: "feedback-store", // unique name for the storage
+      getStorage: () => localStorage, // (optional) by default, 'localStorage' is used
     }
-  },
-}));
+  )
+);
 
 export default useStore;
